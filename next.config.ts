2a1845d84a263c926e64a0b135b1a2c4b26bb556
Yani OULHADJ <yani.oulhadj@johnpaul.com>
2026-02-23 17:06:16 +0100

    chore: update config, layout and global styles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/room/:path*",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
